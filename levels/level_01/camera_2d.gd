d67d1ebb405909140c12d33f801638c10ff8302e
Victor Dante <victordante.designer@gmail.com>
2026-09-22 20:25:23 -0300

    Update camera 2d look down look up

--- levels/level_01/camera_2d.gd
+++ levels/level_01/camera_2d.gd
@@ -1,18 +1,39 @@
 extends Camera2D
 
-@export var look_offset: float = 120.0   # o quanto a câmera se desloca ao olhar pra cima/baixo
-@export var look_speed: float = 6.0      # velocidade de interpolação do offset
+@export var look_offset: float = 120.0
+@export var look_speed: float = 6.0
+@export var hold_threshold: float = 0.15
+
+var _hold_time: float = 0.0
+var _holding_direction: int = 0
+
+@onready var player: CharacterBody2D = get_parent()
 
 func _ready() -> void:
 	position_smoothing_enabled = true
-	position_smoothing_speed = 5.0  # <- esse número É o seu "delayzinho". Menor = mais delay/mole
+	position_smoothing_speed = 5.0
 
 func _process(delta: float) -> void:
+	var input_direction := 0
+
+	if player.is_on_floor():
+		if Input.is_action_pressed("aim_up"):
+			input_direction = -1
+		elif Input.is_action_pressed("aim_down"):
+			input_direction = 1
+
+	if input_direction != 0 and input_direction == _holding_direction:
+		_hold_time += delta
+	elif input_direction != 0:
+		_holding_direction = input_direction
+		_hold_time = 0.0
+	else:
+		_holding_direction = 0
+		_hold_time = 0.0
+
 	var target_offset_y := 0.0
 
-	if Input.is_action_pressed("look_up"):
-		target_offset_y = -look_offset
-	elif Input.is_action_pressed("look_down"):
-		target_offset_y = look_offset
+	if _hold_time >= hold_threshold:
+		target_offset_y = look_offset * _holding_direction
 
 	offset.y = lerpf(offset.y, target_offset_y, look_speed * delta)
